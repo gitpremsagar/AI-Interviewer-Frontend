f008e8515573dc7941a5802cb1db107bc46564b1
gitpremsagar <psagar172@gmail.com>
2024-05-27 15:47:10 +0530

    feat - Create redux store for storing chatHistory

--- src/components/homepage/asideLeft/AsideLeft.tsx
+++ src/components/homepage/asideLeft/AsideLeft.tsx
@@ -1,12 +1,14 @@
 const AsideLeft = () => {
   return (
     <div className="col-span-2 bg-gray-800 text-white min-h-screen p-5">
-      <h1 className="text-xl font-bold text-center">AI Interviewer</h1>
-      <menu className="mt-5">
-        <li className="py-2">Chat 1</li>
-        <li className="py-2">Chat 2</li>
-        <li className="py-2">Chat 3</li>
-      </menu>
+      <div className="fixed">
+        <h1 className="text-xl font-bold text-center">AI Interviewer</h1>
+        <menu className="mt-5">
+          <li className="py-2">Chat 1</li>
+          <li className="py-2">Chat 2</li>
+          <li className="py-2">Chat 3</li>
+        </menu>
+      </div>
     </div>
   );
 };
--- src/types/history.type.ts
+++ src/types/history.type.ts
@@ -4,16 +4,19 @@ const partSchema = z.object({
   text: z.string(),
 });
 
+const rollSchema = z.enum(["user", "model"]);
+
 const historySchema = z.array(
   z.object({
-    role: z.enum(["user", "model"]),
+    role: rollSchema,
     parts: z.array(partSchema),
   })
 );
 
 type Part = z.infer<typeof partSchema>;
 type History = z.infer<typeof historySchema>;
+type Role = z.infer<typeof rollSchema>;
 
-export { historySchema };
+export { historySchema, partSchema, rollSchema };
 
-export type { Part, History };
+export type { Part, History, Role};
